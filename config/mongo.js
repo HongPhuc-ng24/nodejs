2bce7a663c607983f1c54bec6b0a437d741a1ce8
Hong Phuc Nguyen <162108682+HongPhuc-ng24@users.noreply.github.com>
2024-12-07 12:31:56 +0700

    Delete config directory

--- config/mongo.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export function connectdb(){
-    mongoose.connect('mongodb://127.0.0.1:27017/api_booking')
-.then(() => console.log('Connected!'))
-.catch(err => console.error("Connection error:", err));
-}
